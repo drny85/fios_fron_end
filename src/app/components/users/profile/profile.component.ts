e29b34aef951acb4783ad80469a12d2eef4be36c
Robert Melendez <robertm3lendez@gmail.com>
2018-12-20 17:05:24 -0500

    add user page data

--- src/app/components/users/profile/profile.component.ts
+++ src/app/components/users/profile/profile.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../../services/auth/auth.service";
+import { User } from "../../../models/user.model";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  selector: "app-profile",
+  templateUrl: "./profile.component.html",
+  styleUrls: ["./profile.component.css"]
 })
 export class ProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(private AuthService: AuthService) {}
 
   ngOnInit() {
+    this.getUser();
   }
 
+  getUser() {
+    const userId = localStorage.getItem("userId");
+    if (userId) {
+      this.AuthService.getUserById(userId).subscribe(user => console.log(user));
+    }
+  }
 }
